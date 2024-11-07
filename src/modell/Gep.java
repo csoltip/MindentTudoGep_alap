9cb4e4de699803d87093256994735a7b29b19098
CsoltiP <csolti@szamalk-szalezi.hu>
2024-11-07 17:49:02 +0100

    void sorsol() --> boolean[91]

--- src/modell/Gep.java
+++ src/modell/Gep.java
@@ -1,12 +1,35 @@
 package modell;
 
+import java.util.Arrays;
+
 public class Gep {
     private boolean[] lottoszamok;
 
     public Gep() {
         lottoszamok = new boolean[91];
+        Arrays.fill(lottoszamok, false);
+        sorsol();
     }
 
+    public void sorsol(){
+        int db = 0;
+        while(db < 5){
+            int sz = velSzam();
+            if(!lottoszamok[sz]){
+                lottoszamok[sz] = true;
+                db++;
+            }
+        }
+    }
+    
+    private int velSzam(){
+        return velSzam(1, 90);
+    }
+    
+    private int velSzam(int min, int max){
+        return (int)(Math.random()*(max-min+1))+min;
+    }
+    
     public String getNyeroSzamokStr(){
         return "1,2,3,4,5";
     }
